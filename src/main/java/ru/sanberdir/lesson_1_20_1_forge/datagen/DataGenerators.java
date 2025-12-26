f3011205068d129b5b751404c6cfbacb889e7231
Sanberdir <91392232+Sanberdir@users.noreply.github.com>
2025-12-26 20:23:33 +0300

    lesson_39

--- src/main/java/ru/sanberdir/lesson_1_20_1_forge/datagen/DataGenerators.java
+++ src/main/java/ru/sanberdir/lesson_1_20_1_forge/datagen/DataGenerators.java
@@ -20,6 +20,8 @@ public class DataGenerators {
         ExistingFileHelper existingFileHelper = event.getExistingFileHelper();
 
         CompletableFuture<HolderLookup.Provider> lookupProvider = event.getLookupProvider();
+
+        generator.addProvider(event.includeClient(), new ModItemModelProvider(packOutput, existingFileHelper));
         generator.addProvider(event.includeServer(), new ModGlobalLootModifiersProvider(packOutput));
         generator.addProvider(event.includeServer(), new ModPoiTypeTagsProvider(packOutput, lookupProvider, existingFileHelper));
         generator.addProvider(event.includeServer(), new ModWorldGenProvider(packOutput, lookupProvider));
